ed5468778c403bef0fcedf0f6c1d23f4152c53f8
Vishnupriya2207 <v73286328@gmail.com>
2026-07-28 17:33:56 +0530

    Create patientDAO.java

---
+++ patientDAO.java
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Register</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h2>Patient Registration</h2>
+
+<input type="text" placeholder="Name">
+
+<input type="number" placeholder="Age">
+
+<input type="text" placeholder="Gender">
+
+<input type="text" placeholder="Phone">
+
+<input type="email" placeholder="Email">
+
+<input type="password" placeholder="Password">
+
+<button>Register</button>
+
+</div>
+
+</body>
+</html>
