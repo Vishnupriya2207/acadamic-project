2d5dfd816112bda4d8687cb7dd9f54c33ab2abef
Vishnupriya2207 <v73286328@gmail.com>
2026-07-28 17:32:16 +0530

    Create appointmentDAO.java

---
+++ appointmentDAO.java
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hospital Appointment System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <h1>City Care Hospital</h1>
+    <p>Online Appointment Management System</p>
+</header>
+
+<div class="container">
+
+    <h2>Welcome</h2>
+
+    <a href="login.html">
+        <button>Login</button>
+    </a>
+
+    <a href="register.html">
+        <button>Register</button>
+    </a>
+
+</div>
+
+</body>
+</html>
