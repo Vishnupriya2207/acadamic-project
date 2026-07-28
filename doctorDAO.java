df76250adf277b248be252eeb6bda9dbff7d3a76
Vishnupriya2207 <v73286328@gmail.com>
2026-07-28 17:33:05 +0530

    Create doctorDAO.java

---
+++ doctorDAO.java
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Login</title>
+<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<div class="container">
+
+<h2>Patient Login</h2>
+
+<input type="email" placeholder="Email">
+
+<input type="password" placeholder="Password">
+
+<button>Login</button>
+
+</div>
+
+</body>
+</html>
