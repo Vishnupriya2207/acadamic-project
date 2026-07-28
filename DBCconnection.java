9ae92511b3a15111597be271745e6ebc29454df4
Vishnupriya2207 <v73286328@gmail.com>
2026-07-28 17:36:48 +0530

    Create DBCconnection.java

---
+++ DBCconnection.java
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Book Appointment</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+<header>
+    <h1>Book Appointment</h1>
+</header>
+
+
+<div class="container">
+
+<h2>Appointment Details</h2>
+
+
+<input type="text" placeholder="Patient Name">
+
+
+<input type="text" placeholder="Doctor Name">
+
+
+<input type="date">
+
+
+<input type="time">
+
+
+<button>Confirm Appointment</button>
+
+
+</div>
+
+
+</body>
+</html>
